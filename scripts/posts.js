01794b43cf1b8a5ff7798293334706a5f95dc4eb
Deavion <mcintosh.deavion@gmail.com>
2023-01-03 08:51:09 -0500

    Changes to Landing html/css/js & Posts html/css/js

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,1 +1,35 @@
 "use strict";
+
+window.onload = function() {
+    // Check the login status
+    var loggedIn = false;
+    if (localStorage) {
+      loggedIn = localStorage.getItem("loggedIn") === "true";
+    } else {
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        if (cookies[i].trim().startsWith("loggedIn=true")) {
+          loggedIn = true;
+          break;
+        }
+      }
+    }
+}
+
+fetch("https://microbloglite.herokuapp.com/posts")
+.then(response => response.json())
+.then(posts => {
+  const container = document.getElementById("posts-container");
+  let html = "";
+  posts.forEach(post => {
+    html += `<div class="post">
+      <h3>${post.title}</h3>
+      <p>${post.body}</p>
+      <button class="like-button">Like</button>
+      <button class="comment-button">Comment</button>
+      <p>Likes: ${post.likes}</p>
+      <p>Comments: ${post.comments}</p>
+    </div>`;
+  });
+  container.innerHTML = html;
+});
