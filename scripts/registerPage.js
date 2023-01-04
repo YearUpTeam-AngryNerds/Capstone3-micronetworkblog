cfd876d8d7a8487646f4a4b7d993569513422856
Richard Perez <richard.floresperez0@gmail.com>
2023-01-04 09:35:45 -0500

    Formatting

--- scripts/registerPage.js
+++ scripts/registerPage.js
@@ -1,34 +1,27 @@
 "use strict";
 
 window.onload = () => {
-    document.getElementById("register").onclick = (event) => {
-        console.log("Hello")
-       
-        const fullName = document.getElementById("name").value;
-        const username = document.getElementById("user").value;
-        const password = document.getElementById("pw").value;
-        const anyEmptyValues = Boolean(fullName) && Boolean(username) && Boolean(password);
-        if(anyEmptyValues){
-            const userData = {
-                username: username,
-                fullName: fullName,
-                password: password
-            };
-            const requestBody = {
-                method: "POST",
-                headers: {  "Content-type" :"application/json" },
-                body: JSON.stringify(userData)
-            };
-            fetch(`${api}/api/users`, requestBody)
+    document.getElementById("registerForm").onsubmit = (event) => {
+        //prevent the form from refreshing the page
+        event.preventDefault();
+
+        const userData = {
+            username: username,
+            fullName: fullName,
+            password: password
+        };
+        const requestBody = {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(userData)
+        };
+        fetch(`${api}/api/users`, requestBody)
             .then(response => response.json)
             .then(data => {
                 window.location.assign("login.html");
             })
             .catch(err => console.log("Something went bad"));
-        } else {
-            //if the user doesn't fill in any of the input fields
-            console.log("Give me complete input");
-        }
-            
+
+
     }
 }
